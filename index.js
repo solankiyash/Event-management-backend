22a435629a48f8fe7544b7599398b73d58816855
solankiyash <84774663+solankiyash@users.noreply.github.com>
2025-02-02 23:08:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // cors for sequrity allow
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://event-management-backend-752z.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
